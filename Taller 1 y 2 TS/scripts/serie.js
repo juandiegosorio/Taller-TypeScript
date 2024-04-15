4a59e404a21b18b0426693c8748b3188102e766d
Juan Diego Osorio <jd.osorioc1@uniandes.edu.co>
2024-04-14 23:50:49 -0500

    Taller 1 y 2 de Typescript

---
+++ Taller 1 y 2 TS/scripts/serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, titulo, productor, temporadas, descripcion, enlace, imagen) {
+        this.id = id;
+        this.titulo = titulo;
+        this.productor = productor;
+        this.temporadas = temporadas;
+        this.descripcion = descripcion;
+        this.enlace = enlace;
+        this.imagen = imagen;
+    }
+    return Serie;
+}());
+export { Serie };
---
+++ Taller 1 y 2 TS/serie.ts
@@ -0,0 +1,5 @@
+export class Serie{
+    constructor(public id: number , public titulo: string,  public productor: string, public temporadas: number, public descripcion: string, public enlace: string, public imagen: string ){
+
+    }
+}
